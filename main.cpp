89c06407b4cfb40650fc41b8ec64550dc0165b5e
fuse <fzorto@gmail.com>
2020-01-12 14:26:27 -0300

    add spline paths

--- main.cpp
+++ main.cpp
@@ -3,12 +3,15 @@
 #include "shaderprogram.h"
 #include "pixmap.h"
 #include "texture.h"
+#include "geometry.h"
 
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
 #include <glm/vec2.hpp>
 #include <glm/gtc/matrix_transform.hpp>
+#define GLM_ENABLE_EXPERIMENTAL
+#include <glm/gtx/string_cast.hpp>
 
 #include <array>
 #include <vector>
@@ -18,6 +21,101 @@
 #include <iostream>
 
 using QuadVerts = std::array<glm::vec2, 4>;
+
+struct PathSegment
+{
+    glm::vec2 eval(float t) const;
+    std::array<glm::vec2, 4> control_points;
+};
+
+using Path = std::vector<PathSegment>;
+
+class Trajectory
+{
+public:
+    Trajectory(const Path &path);
+    float length() const { return length_; }
+
+    glm::vec2 point_at(float distance) const;
+
+private:
+    struct Vertex
+    {
+        float distance;
+        glm::vec2 position;
+    };
+    std::vector<Vertex> verts_;
+    float length_;
+};
+
+glm::vec2 PathSegment::eval(float t) const
+{
+    const auto c0 = (1.0f - t) * (1.0f - t) * (1.0f - t);
+    const auto c1 = 3.0f * (1.0f - t) * (1.0f - t) * t;
+    const auto c2 = 3.0f * (1.0f - t) * t * t;
+    const auto c3 = t * t * t;
+    return c0 * control_points[0] + c1 * control_points[1] + c2 * control_points[2] + c3 * control_points[3];
+}
+
+Trajectory::Trajectory(const Path &path)
+    : length_(0.0f)
+{
+    constexpr const auto VertsPerSegment = 20;
+    verts_.reserve(path.size() * VertsPerSegment);
+
+    auto add_vertex = [this, first = true, prev_pos = glm::vec2()](const glm::vec2 &pos) mutable {
+        if (!first)
+            length_ += glm::distance(prev_pos, pos);
+        verts_.push_back({length_, pos});
+        prev_pos = pos;
+        first = false;
+    };
+
+    for (const auto &segment : path)
+    {
+        for (int i = 0; i < VertsPerSegment; ++i)
+        {
+            const auto t = static_cast<float>(i) / VertsPerSegment;
+            const auto pos = segment.eval(t);
+            add_vertex(pos);
+        }
+    }
+
+    if (!path.empty())
+    {
+        const auto &pos = path.back().control_points[3];
+        add_vertex(pos);
+    }
+
+    for (size_t i = 0; i < verts_.size(); ++i)
+    {
+        const auto &v = verts_[i];
+        std::cout << i << ": " << glm::to_string(v.position) << ' ' << v.distance << '\n';
+    }
+}
+
+glm::vec2 Trajectory::point_at(float distance) const
+{
+    if (distance <= 0.0f)
+        return verts_.front().position;
+    if (distance >= length_)
+        return verts_.back().position;
+    auto it = std::lower_bound(verts_.begin(), verts_.end(), distance, [](const Vertex &vert, float distance) {
+        return vert.distance < distance;
+    });
+    assert(it != verts_.begin() && it != verts_.end());
+
+    std::cout << distance << ' ' << std::distance(verts_.begin(), it) << '\n';
+
+    const auto &v1 = *it;
+    const auto &v0 = *std::prev(it);
+
+    assert(distance >= v0.distance);
+    assert(distance < v1.distance);
+    const float t = (distance - v0.distance) / (v1.distance - v0.distance);
+
+    return v0.position + t * (v1.position - v0.position);
+}
 
 struct TextureSheet;
 
@@ -164,8 +262,6 @@ void SpriteBatcher::render_batch() const
     glUnmapBuffer(GL_ARRAY_BUFFER);
     do_render();
 
-    std::cout << "draw calls: " << draw_calls << '\n';
-
     glBindVertexArray(0);
     glBindBuffer(GL_ARRAY_BUFFER, 0);
 }
@@ -239,7 +335,7 @@ int main()
             auto pm = load_pixmap_from_png(image);
             sheet.texture->set_data(*pm);
 
-            sheet.tiles.push_back({"test", {{{0, 0}, {1, 0}, {1, 1}, {0, 1}}}, &sheet});
+            sheet.tiles.push_back({"test", {{{0, 1}, {0, 0}, {1, 0}, {1, 1}}}, &sheet});
         };
 
         TextureSheet sheet0;
@@ -254,20 +350,61 @@ int main()
         SpriteBatcher batcher;
         batcher.set_view_rectangle(0, window_width, 0, window_height);
 
+        PathSegment segment{{{{-20, 160}, {440, 120}, {360, 300}, {-20, 440}}}}; // omg so many braces
+
+        std::vector<std::tuple<glm::vec2>> verts;
+
+        constexpr const auto NumVerts = 21;
+        for (int i = 0; i < NumVerts; ++i)
+        {
+            const auto t = static_cast<float>(i) / (NumVerts - 1);
+            const auto v = segment.eval(t);
+            verts.emplace_back(v);
+            std::cout << v.x << ' ' << v.y << '\n';
+        }
+
+        Geometry<std::tuple<glm::vec2>> g;
+        g.set_data(verts);
+
+        ShaderProgram program;
+        program.add_shader(GL_VERTEX_SHADER, "resources/shaders/dummy.vert");
+        program.add_shader(GL_FRAGMENT_SHADER, "resources/shaders/dummy.frag");
+        program.link();
+
+        const auto projection_matrix =
+            glm::ortho(0.0f, static_cast<float>(window_width),
+                       0.0f, static_cast<float>(window_height));
+
+        program.bind();
+        program.set_uniform(program.uniform_location("mvp"), projection_matrix);
+
+        Trajectory trajectory({segment});
+
+        float distance = 0.0f;
+
         while (!glfwWindowShouldClose(window))
         {
             glClearColor(0, 0, 0, 0);
             glClear(GL_COLOR_BUFFER_BIT);
 
+            program.bind();
+            g.render(GL_LINE_STRIP);
+
+            const auto pos = trajectory.point_at(distance);
+            const auto hsize = 40.f;
+
             batcher.start_batch();
-            batcher.add_sprite(tile0, {{{0, 0}, {0, 80}, {80, 80}, {80, 0}}}, 0);
-            batcher.add_sprite(tile0, {{{40, 40}, {40, 120}, {120, 120}, {120, 40}}}, 1);
-            batcher.add_sprite(tile1, {{{80, 80}, {80, 160}, {160, 160}, {160, 80}}}, 0);
-            batcher.add_sprite(tile1, {{{120, 120}, {120, 200}, {200, 200}, {200, 120}}}, 1);
+            batcher.add_sprite(tile0, {
+                    {pos + glm::vec2(-hsize, -hsize),
+                     pos + glm::vec2(-hsize, hsize),
+                     pos + glm::vec2(hsize, hsize),
+                     pos + glm::vec2(hsize, -hsize)}}, 0);
             batcher.render_batch();
 
             glfwSwapBuffers(window);
             glfwPollEvents();
+
+            distance += 1.0f;
         }
     }
 
